3441dfa8c1a5e48cd7c6b15fa875318936d68b20
skeeth <ethanhalbe@gmail.com>
2026-06-27 13:51:57 -0500

    BOID avoid failed attempt

--- Source/Unit/Enemy/enemy.gd
+++ Source/Unit/Enemy/enemy.gd
@@ -6,6 +6,9 @@ var data:EnemyData
 @export var sprite:Sprite2D
 var movespeed:float = 100
 
+@export var boid_strength:float
+var neighbors:Array[Enemy]
+
 const _self_scene = preload("uid://4ppu28orsq7")
 
 static func create(from_data:EnemyData) -> Enemy:
@@ -27,16 +30,47 @@ func _ready() -> void:
 	navi.target_position = Vector2.ZERO
 	died.connect(on_death)
 	
-	
+
 func _physics_process(delta: float) -> void:
 	if navi.is_target_reached():
 		return
-	var diff = navi.get_next_path_position() - global_position
+	#var avoid:Vector2 = Vector2.ZERO
+	#
+	#for neighbor in neighbors:
+		#if !neighbor: continue
+		#var aa:Vector2 = Vector2.ZERO
+		#aa = (global_position - neighbor.global_position)
+		#aa *= lerp(5.0,0.1,avoid.length()/data.size)
+		#avoid += aa
+	#
+	#avoid *= boid_strength
+	#avoid = avoid.clamp(Vector2(-movespeed,-movespeed),Vector2(movespeed,movespeed))
+	var nav_diff = navi.get_next_path_position() - global_position
+	var target_position:Vector2 = global_position + nav_diff# + avoid
+	
+	var diff = target_position - global_position
 	global_position += diff.normalized() * movespeed * delta
-	navi.velocity = diff.normalized() * movespeed * delta
+	#navi.velocity = diff.normalized() * movespeed * delta
 	
 
 func on_death(_self):
 	assert(_self == self)
 	queue_free()
 	
+
+
+#func _on_boid_area_area_entered(area: Area2D) -> void:
+	#var parent = area.get_parent()
+	#assert(parent is Enemy)
+	#if parent is Enemy:
+		#parent.died.connect(neighbors.erase)
+	#neighbors.append(parent)
+
+
+#func _on_boid_area_area_exited(area: Area2D) -> void:
+	#var parent = area.get_parent()
+	#if parent is Enemy:
+		#if parent.died.is_connected(neighbors.erase):
+			#parent.died.disconnect(neighbors.erase)
+	#
+	#neighbors.erase(area.get_parent())
