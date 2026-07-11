e90a61922e905a427ae263e7d96ad9a83b98311d
Eth <ethanhalbe@gmail.com>
2026-07-11 02:01:29 -0500

    BEDTIME OR WHATEVER

--- Source/Level/Title/title.gd
+++ Source/Level/Title/title.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 
 @export var panel_sequence:Array[PanelContainer]
+var panel_index:int = 0
+@onready var tutorial_panel: PanelContainer = $"Tutorial Panel"
+
+
 
 func _ready() -> void:
 	for i in ResourceManager.ResourceTypes.values():
@@ -14,4 +18,19 @@ func _on_start_game_pressed() -> void:
 
 
 func _on_help_pressed() -> void:
-	pass # Replace with function body.
+	tutorial_panel.visible = !tutorial_panel.visible
+	next_slide()
+
+
+func _on_tutorial_panel_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("lmb"):
+		next_slide()
+
+func next_slide():
+	panel_sequence[panel_index-1].hide()
+	panel_sequence[panel_index].show()
+	if panel_index + 1 == panel_sequence.size():
+		panel_index = 0
+	else:
+		panel_index += 1
+	
